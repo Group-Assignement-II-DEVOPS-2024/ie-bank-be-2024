fd5c3930ef1cd902521465bd9aa2efed5af5a8cf
Jannikwendt <jwendt.ieu2021@student.ie.edu>
2024-12-05 19:25:35 +0100

    updated config for test db

--- config.py
+++ config.py
@@ -1,31 +1,32 @@
 import os
 
-class Config(object): 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = False
+class Config(object):
+   SQLALCHEMY_TRACK_MODIFICATIONS = False 
+   DEBUG = False
 
 class LocalConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///local.db'
-    DEBUG = True
+   SQLALCHEMY_DATABASE_URI = 'sqlite:///local.db'
+   DEBUG = True
 
 class GithubCIConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
-    DEBUG = True
+   SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"  # In-memory SQLite for tests
+   TESTING = True
+   DEBUG = True
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.getenv('DBUSER'),
-    dbpass=os.getenv('DBPASS'),
-    dbhost=os.getenv('DBHOST'),
-    dbname=os.getenv('DBNAME')
-    )
-    DEBUG = True
+   SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+       dbuser=os.getenv('DBUSER'),
+       dbpass=os.getenv('DBPASS'), 
+       dbhost=os.getenv('DBHOST'),
+       dbname=os.getenv('DBNAME')
+   )
+   DEBUG = True
 
 class UatConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.getenv('DBUSER'),
-    dbpass=os.getenv('DBPASS'),
-    dbhost=os.getenv('DBHOST'),
-    dbname=os.getenv('DBNAME')
-    )
-    DEBUG = True
+   SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+       dbuser=os.getenv('DBUSER'),
+       dbpass=os.getenv('DBPASS'),
+       dbhost=os.getenv('DBHOST'), 
+       dbname=os.getenv('DBNAME')
+   )
+   DEBUG = True
