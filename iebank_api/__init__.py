1ac063a55f478fa10164381834c03a7e056149e1
Jannikwendt <jwendt.ieu2021@student.ie.edu>
2024-12-05 19:18:00 +0100

    updated init.py for database configuration

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -5,12 +5,11 @@ import os
 
 app = Flask(__name__)
 
-
 # Select environment based on the ENV environment variable
 if os.getenv('ENV') == 'local':
     print("Running in local mode")
     app.config.from_object('config.LocalConfig')
-elif os.getenv('ENV') == 'dev':
+elif os.getenv('ENV') == 'development':
     print("Running in development mode")
     app.config.from_object('config.DevelopmentConfig')
 elif os.getenv('ENV') == 'ghci':
@@ -20,12 +19,14 @@ elif os.getenv('ENV') == 'uat':
     print("Running in uat mode")
     app.config.from_object('config.UatConfig')
 
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DBUSER')}:{os.getenv('DBPASS')}@{os.getenv('DBHOST')}/{os.getenv('DBNAME')}"
+
 db = SQLAlchemy(app)
 
 from iebank_api.models import Account
-
 with app.app_context():
     db.create_all()
+
 CORS(app)
 
 from iebank_api import routes
