ad28a89ef37062ab4ccea6b4f9da7863f0d8417f
Jannikwendt <jwendt.ieu2021@student.ie.edu>
2024-12-07 22:43:14 +0100

    f me

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import os
 from iebank_api import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable from Azure or default to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True)
