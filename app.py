6578cf7f555da8e0e698eec8ef776dbc0f9846f2
Jannikwendt <jwendt.ieu2021@student.ie.edu>
2024-12-07 21:53:58 +0100

    im done with this bs error

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-from flask_cors import CORS
 from iebank_api import app
-
-# Enable CORS for the frontend domain
-CORS(app, origins=["https://jwendt-fe-dev.azurewebsites.net"], supports_credentials=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
